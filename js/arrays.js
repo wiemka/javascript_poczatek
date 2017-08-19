36f81a8ffedf2383393887b6a07d8c5a73bd0a3c
Magda Wielgosz <wielgosz.m@vp.pl>
2017-08-19 12:20:38 +0200

    ćwiczenia z tablicami

--- js/arrays.js
+++ js/arrays.js
@@ -50,3 +50,29 @@ var imionaMeskie = ["Wojtek", "Michał", "Mikołaj"];
 var zbiorImion = imionaMeskie.concat(imiona);
 console.log(zbiorImion);
 console.log("Tablica imiona męskie: " + imionaMeskie.join(", "));
+
+console.log("**********************");
+console.log(zbiorImion.indexOf("Michał"));
+/*pod jakim indexem jest dana wartość, pierwszy napotkany element
+jak jest -1 to znaczy, że nie ma takiego elementu*/
+
+console.log("**********************");
+console.log(Array.isArray(zbiorImion));
+/*sprawdza czy zbiorImion jest tablicą*/
+
+console.log("**********************");
+console.log(zbiorImion.slice(2, 5));
+/*bierze index 2, 3, 4 a 5 już nie bieże => zbior jednostronnie zamknięty <2,5)*/
+
+console.log("**********************");
+console.log(zbiorImion);
+
+
+zbiorImion.splice(2, 3, "Zenek", "Grzesiek", "Ania", 
+"Marian"); /*usuwa trzy elementy od indeksu 2*/
+console.log(zbiorImion);
+
+/*zbiorImion.splice(2); usuwa wszystko od indexu 2 do końca
+console.log(zbiorImion);*/
+
+console.log(zbiorImion.toString()); /*nie można podać czym rozdzielamy*/
