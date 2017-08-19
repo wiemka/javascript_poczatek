966d718cace1b92c8662932c7bdc6de9e1250fe2
Magda Wielgosz <wielgosz.m@vp.pl>
2017-08-19 09:44:41 +0200

    zabawy ze zmiennymi

---
+++ js/variables.js
@@ -0,0 +1,20 @@
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+console.log(imie);
+console.log(imieDamskie);
+console.log(wiek);
+
+imie = "Adam";
+imieDamskie = "Zosia";
+console.log("****************");
+console.log(imie);
+console.log(imieDamskie);
+console.log(wiek);
+
+imie = imieDamskie;
+console.log("****************");
+console.log(imie);
+console.log(imieDamskie);
+console.log(wiek);
